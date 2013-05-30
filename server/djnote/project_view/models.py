3c079d49b07b6c34912463147d2dd6c9b2f064ba
unknown <patrickoneil.music@gmail.com>
2013-05-30 10:22:15 -0700

    Redesigned url confs, reworked profile page

--- server/djnote/project_view/models.py
+++ server/djnote/project_view/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Project(models.Model):
-	name = models.CharField(max_length=40)
+	name = models.CharField(max_length=40, unique=True)
 	ownerz = models.ManyToManyField(User, related_name='user_projects')
 	tracks = models.ForeignKey('Track')
 
