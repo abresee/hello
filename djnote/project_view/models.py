aa7ade56486a954f832d0377717473d2ee1266e2
brain-deaf <patrickoneil.music@gmail.com>
2013-06-02 21:47:45 -0700

    added event_window creation

--- djnote/project_view/models.py
+++ djnote/project_view/models.py
@@ -2,16 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Project(models.Model):
-	name = models.CharField(max_length=40)
-	ownerz = models.ManyToManyField(User)
+    name = models.CharField(max_length=40)
+    ownerz = models.ManyToManyField(User)
 
 class Event_Window(models.Model):
-	window_position = models.IntegerField()
-	window_length = models.IntegerField()
-	window_track = models.IntegerField()
+    window_position = models.IntegerField()
+    window_length = models.IntegerField()
+    id_number = models.IntegerField()
+    project = models.ForeignKey('Project')
+    track = models.ForeignKey('Track')
+   
 
 class Track(models.Model):
-	track_number = models.IntegerField()
-	project = models.ForeignKey('Project')
+    track_number = models.IntegerField()
+    project = models.ForeignKey('Project')
 
-	
+    
--- djnote/project_view/urls.py
+++ djnote/project_view/urls.py
@@ -7,6 +7,10 @@ urlpatterns = patterns('',
 	url(r'^(?P<project_name>[\w\s]+)/$', views.index, name='index'),
 	url(r'^(?P<project_name>[\w\s]+)/create_track/$', views.track_creation, name='track_creation'),
 	url(r'^(?P<project_name>[\w\s]+)/delete_track/$', views.track_deletion, name='track_deletion'),
+    url(r'^(?P<project_name>[\w\s]+)/create_event_window/$', views.event_container_creation, name='event_window'),
+    url(r'^(?P<project_name>[\w\s]+)/event_window_dragstop/$', views.event_container_dragstop, name='event_window_dragstop'),
+    url(r'^(?P<project_name>[\w\s]+)/event_window_resizestop/$', views.event_container_resizestop, name='event_window_resizestop'),
 	url(r'^(?P<project_name>[\w\s]+)/save/$', views.projectz_save, name='project_save'),
+    
 			
 )
