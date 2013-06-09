b5a11fd73cfbee606bc2aa329bdcd850ecf05fa4
abresee <bresee.al@husky.neu.edu>
2013-06-09 14:19:52 -0400

    python versions of demos

--- core/chromatic.py
+++ core/chromatic.py
@@ -7,7 +7,7 @@ wg = WaveSynth()
 
 note_length = core_py.sample_rate//8
 note_intensity = core_py.max_intensity//4
-notes = [Note(i, -1, note_intensity, note_length*i, note_length*(i+1)) for i in range(50)]
+notes = [Note(i, -1, note_intensity, note_length*i, note_length) for i in range(50)]
 
 for note in notes:
     wg.add_note(note)
